d92e092b883303b5765ddbe83ade5ce7d9cf496b
Account <account@Accounts-MacBook-Air.local>
2022-04-04 23:14:30 +0500

    last ball ui

--- src/pages/MatchScoreComponent.jsx
+++ src/pages/MatchScoreComponent.jsx
@@ -1,5 +1,40 @@
 export default function MatchScoreComponent() {
     return <div className="container score-container">
+        <CriclyticsBar />
+        <div className="player-info-wrapper">
+            <div className="row">
+                <div className="col-sm-6">
+                    <TitleWithArrow />
+                    <div className="live-balls d-flex justify-content-between">
+                        <div className="ball">6</div>
+                    </div>
+                    <TitleWithArrow />
+                    <div className="projected-status d-flex justify-content-between">
+                        <span>Live</span>
+                        <span>Team</span>
+                        <span>Projected</span>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="live-player-projections">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
+}
+
+
+function TitleWithArrow() {
+    return <div className="grey-bar d-flex justify-content-between">
+        <h2>Last 10 Balls</h2>
+        <img srcSet="https://www.cricket.com/svgs/RightSchevronBlack.svg" alt="" />
+    </div>
+}
+
+function CriclyticsBar() {
+    return <>
         <div className="black-bar d-flex justify-content-between">
             <div className="analytics d-flex align-items-center">
                 <img srcSet="https://www.cricket.com/footer/criclytics.svg" alt="" />
@@ -10,5 +45,6 @@ export default function MatchScoreComponent() {
                 <img srcSet="https://www.cricket.com/svgs/RightSchevronWhite.svg" alt="" />
             </a>
         </div>
-    </div>;
+
+    </>
 }
